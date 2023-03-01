adbc98ca1cf642dfea9563a0eb06c3a6cb310e13
Jayder Caceres <caceresjayder@gmail.com>
2023-03-01 00:06:12 -0500

    [mod] db_config

--- src/config/sequelizeDB.ts
+++ src/config/sequelizeDB.ts
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(db_url)
 const DB_Connect = async () => {
     try{
         const { rows }: any = await sequelize.query("SELECT NOW();")
-        console.log(`Succesfully Connected to database and the hour is ${rows[0].now}`)
+        console.log(`Succesfully Connected to database and the hour is ${rows}`)
     }
     catch(err){
         console.error("Connection with database: errored", err)
