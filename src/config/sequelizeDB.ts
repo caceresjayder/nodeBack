16db3c99302ac8f244b099991391881cc69b275a
Jayder Caceres <caceresjayder@gmail.com>
2023-02-28 23:04:23 -0500

    [mod] db_con

--- src/config/sequelizeDB.ts
+++ src/config/sequelizeDB.ts
@@ -1,16 +1,14 @@
 import { Sequelize } from "sequelize";
 require("dotenv").config();
-console.log("db_variable is: ", process.env.DB_URL)
 const db_url = process.env.DB_URL as string
-console.log("const: ", db_url)
 
 
 const sequelize = new Sequelize(db_url)
 
 const DB_Connect = async () => {
     try{
-        const result = await sequelize.authenticate()
-        console.log(`The result is ${result}`)
+        await sequelize.authenticate()
+        console.log(`Succesfully Connected to database`)
     }
     catch(err){
         console.error("Connection with database: errored", err)
